b31ebf6c3d31c0f90541013e7ca8dba29cefd551
MelisaZobali <melisaazobali@gmail.com>
2026-05-11 14:14:33 +0300

    bağlantı dosyası düzletme

--- baglanti.php
+++ baglanti.php
@@ -7,14 +7,15 @@ $veritabani = "sanat_galerisi_db";
 $baglan = mysqli_connect($host, $kullanici, $sifre, $veritabani);
 
 if (!$baglan) {
-    die("Veritabani bağlantisi başarisiz: " . mysqli_connect_error());
+    die("Veritabanı bağlantısı başarısız: " . mysqli_connect_error());
 }
+
 // Türkçe karakter sorunu yaşamamak için:
 mysqli_set_charset($baglan, "utf8");
-?>
 
-/*Sistemdeki tüm tablolar InnoDB motoru kullanilarak
- Foreign Key (Yabanci Anahtar) ilişkileriyle 
- birbirine bağlanmiştir. Bu sayede veri bütünlüğü
-  korunmakta ve ilişkisel sorgular optimize edilmektedir.
-  */
+/* RAPOR NOTU:
+Sistemdeki tüm tablolar InnoDB motoru kullanılarak 
+Foreign Key (Yabancı Anahtar) ilişkileriyle birbirine bağlanmıştır. 
+Bu sayede veri bütünlüğü korunmakta ve ilişkisel sorgular optimize edilmektedir.
+*/
+?>
